86af9e7579ab153777d6543766028c8812427130
rishaansinghsahoo <rishaansinghsahoo@gmail.com>
2024-08-03 10:39:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 rishaan My new app")
+st.title("Rishaan first app ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
