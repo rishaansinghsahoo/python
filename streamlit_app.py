ce9b1fe44b6f44c85dd0d74fb99802f034846103
rishaansinghsahoo <rishaansinghsahoo@gmail.com>
2024-08-04 10:27:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ st.write
 st.write (" 10*100000")
 st.write( 10*100000)
 
-number1=st.number_input("Insert number 1")
-number2=st.number_input("Insert number 2")
+number1=st.number_input("Insert number 1",step=1)
+number2=st.number_input("Insert number 2",step=1)
 st.write("the sum is ")
 st.write(number1+number2)
 st.write("the product is ")
