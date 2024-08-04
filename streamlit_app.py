053b5164edc3c62561135f14f63a0b60c2ab2b6a
rishaansinghsahoo <rishaansinghsahoo@gmail.com>
2024-08-04 10:27:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
 st.title("Rishaan first app ")
-st.write(
-    "This is my first app, I will make a calculator"
-st. write     
-)
+st.write("This is my first app, I will make a calculator")
 st.write 
 st.write (" 10*100000")
 st.write( 10*100000)
